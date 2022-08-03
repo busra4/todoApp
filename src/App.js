bd87d5b0d439e6d64283eba1e8d66488c94d9782
busra4 <76951800+busra4@users.noreply.github.com>
2022-08-03 14:29:52 +0300

    Update App.js
    removed objects created for test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Form from "./components/Forms";
-import TodoList from "./components/TodoList";
 import Footer from "./comps/Footer";
 import Forms from "./comps/Forms";
 import Lists from "./comps/Lists";
@@ -11,9 +9,7 @@ function App() {
   const [form, setForms] = useState([]);
   const [status, setStatus] = useState("");
   const [filteredTodos, setFilteredTodos] = useState([]);
-  // console.log(Object.keys(todos));
-  // console.log(status);
-  // const deleteToDo=
+
 
   useEffect(() => {
     const filterHandler = () => {
@@ -63,25 +59,3 @@ function App() {
 }
 
 export default App;
-
-/*  <Form todos={todos} setTodos={setTodos} todo={todos.todo}/>
-
-      <ul className="todo-list">
-        {filteredTodos.map((todo) => (
-          <TodoList
-          status={status}
-          key={todo.id}
-          todo={todo}
-          text={todo.text}
-          todos={todos}
-          setTodos={setTodos}
-          />
-          ))}
-      </ul>
-     
-          <Footer
-          status= {status}
-          setStatus={setStatus}
-          todos={todos}
-          setTodos={setTodos}
-          />*/
